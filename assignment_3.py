9e218c38e82e1fa5ee8da4cee4b14710e020f500
[Alex] <aedoler@gmail.com>
2016-02-21 03:04:42 -0300

    updating

--- assignment_3.py
+++ assignment_3.py
@@ -1,21 +1,26 @@
 #!user/bin/env python
 # -*- coding: utf-8 -*-
+"""Downloads and processes data from a website and returns hit information"""
 
 import argparse
 import urllib2
 import csv
 import re
 import decimal
+import os
 
 
 def main():
-    """Docstring"""
+    """Calls all functions"""
     parser = argparse.ArgumentParser()
     parser.add_argument('--url')
     args = parser.parse_args()
-    csvData = downloadData(args.url) # delete later
-    memoryData = yieldData(csvData)
-    searchData(memoryData)
+    if os.path.exists('hitdata.csv'):
+        searchImage('hitdata.csv')
+        searchBrower('hitdata.csv')
+    else:
+        csvData = downloadData(args.url)
+        downloadData(csvData)
 
 
 def downloadData(url):
@@ -29,61 +34,89 @@ def downloadData(url):
         :rtype: object
     """
     response = urllib2.urlopen(url)
+    html = response.read()
+    localfile = open('hitdata.csv', 'wb')
+    localfile.write(html)
+    localfile.close()
 
-    return response
 
-def yieldData(csvData):
-    """Stores download csv data as generator object"""
-    cr = csv.reader(csvData)
+def searchImage(csvfile):
+    """ processes data for images and browser
+    Args:
+        csvfile: file saved locally
+    Returns:
+        str message with number of image hits and percent
+    Example:
+        >>>searchImage(csvfile)
+        6574 hits were for images. Out of a total of 10000,
+        this means images account for 6.5740% of all hits.
+    """
+    f = open(csvfile, 'rb')
+    reader = csv.reader(f)
 
-    for line in cr:
-        yield line
-
-def searchData(memoryData):
-    """ processes data for images and browser"""
     count = 0
     count_all = 0
     pattern = '(?i)(png|jpg|gif)$'
+
+    for row in reader:
+        image = row[0]
+        count_all += 1
+        if re.search(pattern, image) is not None:
+            count += 1
+
+    percent = decimal.Decimal((
+                                  decimal.Decimal(count)
+                                  / decimal.Decimal(count_all)
+                              ) * decimal.Decimal('10'))
+    message = '-------------------------------------------------------------' \
+              '\n{} hits were for images. Out of a total of ' \
+              '{}, this means images account for' \
+              ' {}% of all' \
+              ' hits.'.format(count, count_all, percent)
+
+    print message
+    f.close()
+
+def searchBrower(csvfile):
+    """Processes browser data.
+    Args:
+        csvfile: csv file to processed
+    Returns:
+        str message of broweser most used
+    Examples:
+        >>>searchBrower(csvfile)
+        The browser with the most amount of hits is Chrome,
+         with 4373 hits on the given day
+    """
+    f = open(csvfile, 'rb')
+    reader = csv.reader(f)
 
     firefox_count = 0
     chrome_count = 0
     safari_count = 0
     explorer_count = 0
 
-    for row in memoryData:
-        image = row[0]
-        count_all += 1
-        if re.search(pattern, image) is not None:
-            count += 1
-
-        image2 = row[2]
-        if re.search('(?i)(firefox)', image2):
+    for row in reader:
+        browser = row[2]
+        if re.search('(?i)(firefox)', browser):
             firefox_count += 1
-        elif re.search('(?i)(chrome)', image2):
+        elif re.search('(?i)(chrome)', browser):
             chrome_count += 1
-        elif re.search('(?i)(safari)', image2):
+        elif re.search('(?i)(safari)', browser):
             safari_count += 1
-        elif re.search('(?i)(trident)', image2): # trident seems to represent IE????
+        elif re.search('(?i)(trident)', browser): # trident seems to represent IE????
             explorer_count += 1
-
-
-    percent = decimal.Decimal((
-                                  decimal.Decimal(count)
-                                  / decimal.Decimal(count_all)
-                              ) * decimal.Decimal('10'))
-    message = '{} hits were for images. Out of a total of ' \
-              '{}, this means images account for' \
-              ' {}% of all' \
-              ' hits.'.format(count, count_all, percent)
 
     get_most_hits = {'Firefox': firefox_count, 'Safari': safari_count, 'Chrome': chrome_count,
                      'Internet Explorer': explorer_count}
     max_key = max(get_most_hits, key=get_most_hits.get)
     max_value = max(get_most_hits.values())
-    browser_message = 'The browser with the most amount of hits is {}, with ' \
-                      '{} hits on the given day'.format(max_key, max_value)
-    print message
+    browser_message = '\nThe browser with the most amount of hits is {}, with ' \
+                      '{} hits on the given day\n----------------------------------' \
+                      '----------------------------' \
+                      ''.format(max_key, max_value)
     print browser_message
+    f.close()
 
 
 if __name__ == '__main__':
